4983fce2152956e5ae0a64f8b3d01c76f03c1a71
Kelly Cook <kellyrcook1980@gmail.com>
2020-12-20 16:13:07 -0700

    Update files with pseudo code to begin planning each step

---
+++ script.js
@@ -0,0 +1,14 @@
+//countdown timer ... 30 second quiz
+
+//questions to populate - array
+
+//options to choose from on the buttons
+
+//show correct or wrong with selection ... can we figure out associated sound effects to go with this?
+
+//scoring mechanism ... number answered correct + time remaining on the clock
+
+//Option to enter name when finished to store on leader board
+
+//leader board with clear option
+
