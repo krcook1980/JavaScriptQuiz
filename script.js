3358bade53f95cb39a91df28b5b36556ced69541
Kelly Cook <kellyrcook1980@gmail.com>
2021-01-01 14:49:05 -0700

    corrected add to local storage function, still need scores to render in list

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //On page open, start button in body to be replaced by quiz, start button prompts timer to begin
 
-
 //global variables
 var timeEl = document.querySelector(".time");
 var start = document.getElementById("startBtn");
@@ -8,7 +7,8 @@ var startDiv = document.getElementById("startDiv");
 var quizDiv = document.getElementById("quizDiv");
 var correct = document.getElementById("correct");
 var endScore = document.getElementById("endScore");
-var highScore = document.getElementById("highScore");
+var highScore = document.getElementById("highScores");
+var highList = document.getElementById("highList");
 var btn1 = document.getElementById("C1");
 var btn2 = document.getElementById("C2");
 var btn3 = document.getElementById("C3");
@@ -18,6 +18,7 @@ var choiceButton = document.querySelectorAll(".choiceBtn");
 var timerInterval;
 var submit = document.querySelector(".submitBtn");
 var clear = document.querySelector(".clearBtn");
+var scores = [];
 
 //question objects
 var Q1 = {
@@ -75,7 +76,7 @@ var Q6 = {
   answer: "C1",
 }
 
-//global var dependant on above objects
+//global var dependant on above objects must be below to function
 var questionsArray = [Q1, Q2, Q3, Q4, Q5, Q6];
 var secondsLeft = questionsArray.length * 15;
 var questionIndex = 0;
@@ -97,14 +98,16 @@ function setTime() {
 
 function sendMessage() {
   timeEl.textContent = "Time's Up!";
-  }
+}
 
 function hideCorrect() {
   correct.textContent = " ";
-}  
+}
 
-//toggle start div to quiz div
-function visible() {
+//on click start button, start time and quiz
+function startQuiz() {
+  setTime();
+  quizDisplay();
   startDiv.className = "invisible"
   startDiv.style = "display:none";
   quizDiv.classList.remove("invisible");
@@ -121,49 +124,52 @@ function quizDisplay() {
   document.getElementById("C4").innerHTML = questionsArray[questionIndex].C4;
 
 }
+
 //find which button is selected
 for (var i = 0; i < choiceBtn.length; i++) {
   choiceBtn[i].addEventListener("click", choices);
-  }
+}
 
 function choices() {
   var btnId = this.getAttribute("id");
   isCorrect(btnId);
   questionIndex++;
-  if (questionIndex <= questionsArray.length-1){
-  quizDisplay();
+  if (questionIndex <= questionsArray.length - 1) {
+    quizDisplay();
   }
-  else{
+  else {
     endQuiz();
     timeEl.className = "invisible";
   }
 }
-//function for if selected button is correct answer
+
+//function for if selected button is correct or incorrect answer
 function isCorrect(btnId) {
 
   if (btnId == questionsArray[questionIndex].answer) {
     correct.textContent = "CORRECT!";
-   
+
   }
   else {
     correct.textContent = "Incorrect...";
-    
+
     if (secondsLeft > 15) {
       secondsLeft = secondsLeft - 15;
     }
     else {
-     endQuiz();
+      endQuiz();
     }
-   
-  }
-  
-  //stop timer and pause to show if correct before moving on to next question
-  setTimeout(hideCorrect, 1000);
- 
-}
-//if time is up or all questions answered, score with box
 
-//scoring mechanism time at end, send to local storage
+  }
+
+  //clear correct/incorrect message after one second
+  setTimeout(hideCorrect, 1000);
+
+}
+
+
+
+//Close quiz div and see final score (time is up or all questions answered)
 function endQuiz() {
   quizDiv.classList.remove("visible");
   quizDiv.className = "invisible";
@@ -171,43 +177,62 @@ function endQuiz() {
   endScore.classList.remove("invisible");
   endScore.classList.className = "visible";
   document.getElementById("finalScore").textContent = secondsLeft;
-  //Option to enter name when finished to store on leader board
-  //get class nameEntered and log to local storage with score
+
 }
 
-//on click start button, start time and quiz
-function startQuiz() {
-  setTime();
-  quizDisplay();
-  visible();
-}
-
+//push score and name to local storage
 function logHighScore() {
-  localStorage.setItem("name", document.querySelector(".nameEntered").value);
-  localStorage.setItem("score", secondsLeft);
-  
+  localStorage.setItem("scores", JSON.stringify(secondsLeft + " - " + document.querySelector(".nameEntered").value));
 }
 
+//Get scores out of local storage & add to ul
+function renderScores() {
+  highList.innerHTML = "";
+  
+  for (var i = 0; i < scores.length; i++) {
+    var score = scores[i];
+    var li = document.createElement("li");
+    li.textContent = score;
+    li.setAttribute("data-index", i);
+  }
 
-//SCORE JS
+}
+
+//View high score list
 function highScores() {
+  //on submit, log high score to local storage
   logHighScore();
+
+  //clear area and make high score list visible
   endScore.classList.remove("visible");
   endScore.className = "invisible";
   endScore.style = "display:none";
   highScore.classList.remove("invisible");
   highScore.classList.className = "visible";
-  var retrieve = localStorage.getItem("name");
-  document.querySelector("#logHigh").textContent=retrieve;  
 
+  //pull local storage scores and add them to the list of high scores
+  var storedScores = JSON.parse(localStorage.getItem("scores"));
+  if (storedScores !== null) {
+    scores = storedScores;
+  }
+
+  renderScores();
 }
 
-function clearS () {
-  localStorage.clear();
-}
 
-clear.addEventListener("click",clearS);
+//Click submit to log and display high scores
 submit.addEventListener("click", highScores);
+
+
+//Clear scores button
+function clearS() {
+  localStorage.clear();
+  highList.innerHTML = "";
+}
+
+clear.addEventListener("click", clearS);
+
+
+//Start button for quiz
 start.addEventListener("click", startQuiz);
 
-//leader board with clear option
